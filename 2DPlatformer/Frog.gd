0bb3b30f0bc1ef99ded0dcd2afb267c89b601c32
Pratyush Kamal <pratyushkamal88@gmail.com>
2023-11-24 14:15:52 +0530

    update

---
+++ 2DPlatformer/Frog.gd
@@ -0,0 +1,63 @@
+extends CharacterBody2D
+
+var SPEED = 100
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var chase = false
+var player
+
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
+func _physics_process(delta):
+	#for frog gavity
+	velocity.y += gravity * delta
+	if chase == true:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Jump")
+		player = get_node("../../PlayerNode/Player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = direction.x * SPEED
+	else:
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")
+		velocity.x = 0
+	
+	
+	move_and_slide()
+
+
+func _on_player_detection_body_entered(body):
+	if body.name == "Player":
+		chase = true
+		#print(player.global_position)
+
+
+
+
+func _on_player_detection_body_exited(body):
+	if body.name == "Player":
+		chase = false
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		Game.Gold +=5
+		Utils.saveGame()
+		death()
+
+
+func _on_player_collision_body_entered(body):
+	if body.name == "Player":
+		Game.playerHP -=3
+		death()
+
+
+func death():
+	chase = false
+	get_node("AnimatedSprite2D").play("Death")
+	await get_node("AnimatedSprite2D").animation_finished
+	self.queue_free()
---
+++ 2DPlatformer/main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+func _ready():
+	Utils.loadGame()
+	Game.playerHP = 10
+
+func _on_quit_pressed():
+	get_tree().quit()
+
+
+func _on_play_pressed():
+	get_tree().change_scene_to_file("res://lvl1.tscn")
